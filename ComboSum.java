5f37595ac26543d0ab8d8432f4a0fb2bdb500062
evaachim <44099125+evaachim@users.noreply.github.com>
2020-01-29 22:26:48 -0500

    Update ComboSum.java

--- ComboSum.java
+++ ComboSum.java
@@ -1,4 +1,16 @@
-package ComboSum;
+/**
+* The Combo TargSum Program Finds all of the possible *combinations in the array that add up to a target
+* 
+* @author:  Eva Achim
+* @Occupation: Howard University Student
+* @Student ID: @02903226
+* @Usage: CSCI 363
+* @When: January 18th 2020
+* @Customer: Bernard Wolfolk
+* @Expectations: User interacts with interface
+* @Test Cases in Test Cases.txt
+*/
+package TargComboSum;
 //libraries to be used in this program
 import java.io.*; 
 import java.lang.*; 
@@ -7,8 +19,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
 import java.util.InputMismatchException;
-//combo sum class
+//combo targSum class
 class Combo {
+  //class that finds all of the combos 
   public static List<List<Integer>> getSum(int[] my_arr, int my_target) {
      /**
    * This method is used to create arraylists and calls the CreateCombo method.
@@ -16,11 +29,13 @@ class Combo {
    * @param 2: ret. This is the second paramter to GetSum method. It is the second ArrayList of integers, to be returned.
    * @return int This returns the array with all of the indices whose elements add up to target.
    */
+            //array of the indices of elements that add up to targSum
             List<Integer> add_elem = new ArrayList<Integer>();
+            //the array of arrays
             List<List<Integer>> ret = new ArrayList<List<Integer>>();
             //message for user
             System.out.println("Your Combinations are: ");
-            createCombo(ret, add_elem, my_arr, my_target, 0);
+            createCombo(0, my_arr, my_target, add_elem,ret);
             //check if it is empty and let user know if so. 
             if (ret.isEmpty()){
               System.out.println("Sorry. Your array does NOT have elements that can add up to your desired target! It's the empty set...");
@@ -28,10 +43,12 @@ class Combo {
      //return the result
             return ret;
         }
-         
-        private static void createCombo(List<List<Integer>> ret, List<Integer> add_elem, int[] my_arr, int my_target,int pos) {
+
+
+    private static void createCombo(int pos, int[] my_arr, int my_target,  List<Integer> add_elem,List<List<Integer>> ret) {
             /**
    * This method is used to populate the ret array with all of the indices that add up to my_target.
+   * this method will be private, only for User class 
    * @param 1: n. This is the first parameter of the CreateCombo method. It is the length of the original array. 
    * @param 2: sum. This is the second paramter of the method. It is the sum of the elements in the original array.
    @param 3: i. This is the third parameter of the method. It is an iterator.
@@ -40,30 +57,35 @@ class Combo {
    @param 6: add_elem. This is the 6th parameter of the method. It adds the indices tht add up to target into individual arrays. 
    * @return nothing. This is a void method. 
    */
+   //the length of the array
                 int n = my_arr.length;
-                int sum = 0;
+                int targSum = 0;
                 for (int i: add_elem) {
-                  if (sum<=my_target)
+                  if (targSum<=my_target)
                   {
                     //keep adding elements up until target is reached
-                        sum += my_arr[i];
+                        targSum += my_arr[i];
                   }
                 }
+                //loop to check if target was indeed reached
                     for (int i = pos; i < n; i++)
                     {
-                       if (sum == my_target) {
+                       if (targSum == my_target) {
                          //when target is reached, add the entire array to the array of arrays
                         ret.add(new ArrayList<Integer>(add_elem));
                         return;
                 }
-                 if (sum < my_target)
+                //the base case
+                 if (targSum < my_target)
                 {
                   //or else add elements to each array
                         add_elem.add(i);
-                        //call method here
-                        createCombo(ret, add_elem, my_arr, my_target, i+1);
+                        //recurssively call method here
+                        createCombo(i+1, my_arr, my_target,  add_elem, ret);
                        add_elem.remove(add_elem.size() - 1);
                     }
+                    //end of base clase
                 }
             }
 }
+//end of class
